5ce60d59a5a75bc9f91ce379177e12b832a204be
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2015-11-02 18:36:04 -0800

    Replace the implementation of PyMLState with pystate.Base

--- creator.go
+++ creator.go
@@ -3,6 +3,7 @@ package pymlstate
 import (
 	"fmt"
 	"io"
+	"pfi/sensorbee/py/pystate"
 	"pfi/sensorbee/sensorbee/bql/udf"
 	"pfi/sensorbee/sensorbee/core"
 	"pfi/sensorbee/sensorbee/data"
@@ -32,34 +33,12 @@ var _ udf.UDSLoader = &PyMLStateCreator{}
 //                     array, which length is "batch_train_size". Default is 10.
 func (c *PyMLStateCreator) CreateState(ctx *core.Context, params data.Map) (
 	core.SharedState, error) {
-	var err error
-	mdlPathName := ""
-	if mp, err := params.Get(modulePath); err == nil {
-		if mdlPathName, err = data.AsString(mp); err != nil {
-			return nil, err
-		}
-		delete(params, "module_path")
-	}
-	mn, err := params.Get(moduleNamePath)
-	if err != nil {
-		return nil, err
-	}
-	delete(params, "module_name")
-	moduleName, err := data.AsString(mn)
+	bp, err := pystate.ExtractBaseParams(params, true)
 	if err != nil {
 		return nil, err
 	}
 
-	cn, err := params.Get(classNamePath)
-	if err != nil {
-		return nil, err
-	}
-	delete(params, "class_name")
-	className, err := data.AsString(cn)
-	if err != nil {
-		return nil, err
-	}
-
+	// TODO: extract this code block as ExtractMLParams function
 	batchSize := 10
 	if bs, err := params.Get(batchTrainSizePath); err == nil {
 		var batchSize64 int64
@@ -73,7 +52,7 @@ func (c *PyMLStateCreator) CreateState(ctx *core.Context, params data.Map) (
 		delete(params, "batch_train_size")
 	}
 
-	return NewPyMLState(mdlPathName, moduleName, className, batchSize, params)
+	return New(bp, &MLParams{BatchSize: batchSize}, params)
 }
 
 // LoadState is same as CREATE STATE.
--- state.go
+++ state.go
@@ -6,9 +6,7 @@ import (
 	"fmt"
 	"github.com/ugorji/go/codec"
 	"io"
-	"io/ioutil"
-	"os"
-	"pfi/sensorbee/py"
+	"pfi/sensorbee/py/pystate"
 	"pfi/sensorbee/sensorbee/core"
 	"pfi/sensorbee/sensorbee/data"
 	"sync"
@@ -17,94 +15,47 @@ import (
 var (
 	lossPath = data.MustCompilePath("loss")
 	accPath  = data.MustCompilePath("accuracy")
-
-	ErrAlreadyTerminated = errors.New("PyMLState is already terminated")
 )
 
 // PyMLState is python instance specialized to multiple layer classification.
 // The python instance and this struct must not be coppied directly by assignment
 // statement because it doesn't increase reference count of instance.
 type PyMLState struct {
-	modulePath string
-	moduleName string
-	className  string
-
-	ins *py.ObjectInstance
-
-	bucket    data.Array
-	batchSize int
-
-	rwm sync.RWMutex
+	base   *pystate.Base
+	params MLParams
+	bucket data.Array
+	rwm    sync.RWMutex
 }
 
-type pyMLMsgpack struct {
-	ModulePath string `codec:"module_path"`
-	ModuleName string `codec:"module_name"`
-	ClassName  string `codec:"class_name"`
-	BatchSize  int    `codec:"batch_size"`
+// MLParams is parameters required for
+type MLParams struct {
+	BatchSize int `codec:"batch_size"`
 }
 
-// NewPyMLState creates `core.SharedState` for multiple layer classification.
-func NewPyMLState(modulePathName, moduleName, className string, batchSize int,
-	params data.Map) (*PyMLState, error) {
-	ins, err := newPyInstance("create", modulePathName, moduleName, className, []data.Value{params}...)
+// New creates `core.SharedState` for multiple layer classification.
+func New(baseParams *pystate.BaseParams, mlParams *MLParams, params data.Map) (*PyMLState, error) {
+	b, err := pystate.NewBase(baseParams, params)
 	if err != nil {
 		return nil, err
 	}
 
-	s := &PyMLState{}
-	s.set(ins, modulePathName, moduleName, className, batchSize)
+	s := &PyMLState{
+		base:   b,
+		params: *mlParams,
+		bucket: make(data.Array, 0, mlParams.BatchSize),
+	}
 	return s, nil
-}
-
-// newPyInstance creates a new Python class instance.
-// User must call DecRef method to release a resource.
-func newPyInstance(createMethodName, modulePathName, moduleName, className string, args ...data.Value) (py.ObjectInstance, error) {
-	var null py.ObjectInstance
-	py.ImportSysAndAppendPath(modulePathName)
-
-	mdl, err := py.LoadModule(moduleName)
-	if err != nil {
-		return null, err
-	}
-	defer mdl.DecRef()
-
-	class, err := mdl.GetClass(className)
-	if err != nil {
-		return null, err
-	}
-	defer class.DecRef()
-
-	ins, err := class.CallDirect(createMethodName, args...)
-	return py.ObjectInstance{ins}, err
-}
-
-// set sets or overwrites fields of PyMLState struct.
-// This method steals a reference of ins object.
-// Caller must not call `s.ins.DecRef()` because set calls DecRef for old ins object.
-func (s *PyMLState) set(ins py.ObjectInstance, modulePathName, moduleName, className string,
-	batchSize int) {
-	if s.ins != nil {
-		s.ins.DecRef()
-	}
-
-	s.modulePath = modulePathName
-	s.moduleName = moduleName
-	s.className = className
-	s.ins = &ins
-	s.bucket = make(data.Array, 0, batchSize)
-	s.batchSize = batchSize
 }
 
 // Terminate this state.
 func (s *PyMLState) Terminate(ctx *core.Context) error {
 	s.rwm.Lock()
 	defer s.rwm.Unlock()
-	if s.ins == nil {
-		return nil // This isn't an error in Terminate.
+	if err := s.base.Terminate(ctx); err != nil {
+		return err
 	}
-	s.ins.DecRef()
-	s.ins = nil
+	// Don't set s.base = nil because it's used for the termination detection.
+	s.bucket = nil
 	return nil
 }
 
@@ -112,38 +63,54 @@ func (s *PyMLState) Terminate(ctx *core.Context) error {
 func (s *PyMLState) Write(ctx *core.Context, t *core.Tuple) error {
 	s.rwm.Lock()
 	defer s.rwm.Unlock()
-	if s.ins == nil {
-		return ErrAlreadyTerminated
+	if err := s.base.CheckTermination(); err != nil {
+		return err
 	}
+
 	s.bucket = append(s.bucket, t.Data)
-
-	var err error
-	if len(s.bucket) >= s.batchSize {
-		m, er := s.fit(ctx, s.bucket)
-		err = er
-		s.bucket = s.bucket[:0] // clear slice but keep capacity
-
-		// optional logging, return non-error even if the value does not have
-		// accuracy and loss.
-		if ret, er := data.AsMap(m); er == nil {
-			var loss float64
-			if l, e := ret.Get(lossPath); e != nil {
-				return err
-			} else if loss, e = data.ToFloat(l); e != nil {
-				return err
-			}
-			var acc float64
-			if a, e := ret.Get(accPath); e != nil {
-				return err
-			} else if acc, e = data.ToFloat(a); e != nil {
-				return err
-			}
-			ctx.Log().Debugf("loss=%.3f acc=%.3f", loss/float64(s.batchSize),
-				acc/float64(s.batchSize))
-		}
+	if len(s.bucket) < s.params.BatchSize {
+		return nil
 	}
 
-	return err
+	m, err := s.fit(ctx, s.bucket)
+	prevBucketSize := len(s.bucket)
+	s.bucket = s.bucket[:0] // clear slice but keep capacity
+	if err != nil {
+		ctx.ErrLog(err).WithField("bucket_size", prevBucketSize).
+			Error("pymlstate's training via Write (INSERT INTO) failed")
+		return err
+	}
+
+	// TODO: add option to toggle the following logging
+
+	ret, err := data.AsMap(m)
+	if err != nil {
+		// The following log is optional. So, it isn't a error even if the
+		// result doesn't have accuracy and loss fields.
+		// TODO: write a warning log after the logging option is added.
+		return nil
+	}
+
+	var loss float64
+	if l, e := ret.Get(lossPath); e != nil {
+		// TODO: add warning
+		return nil
+	} else if loss, e = data.ToFloat(l); e != nil {
+		// TODO: add warning
+		return nil
+	}
+
+	var acc float64
+	if a, e := ret.Get(accPath); e != nil {
+		// TODO: add warning
+		return nil
+	} else if acc, e = data.ToFloat(a); e != nil {
+		// TODO: add warning
+		return nil
+	}
+	ctx.Log().Debugf("loss=%.3f acc=%.3f", loss/float64(s.params.BatchSize),
+		acc/float64(s.params.BatchSize))
+	return nil
 }
 
 // Fit receives `data.Array` type but it assumes `[]data.Map` type
@@ -151,9 +118,6 @@ func (s *PyMLState) Write(ctx *core.Context, t *core.Tuple) error {
 func (s *PyMLState) Fit(ctx *core.Context, bucket data.Array) (data.Value, error) {
 	s.rwm.RLock()
 	defer s.rwm.RUnlock()
-	if s.ins == nil {
-		return nil, ErrAlreadyTerminated
-	}
 	return s.fit(ctx, bucket)
 }
 
@@ -163,7 +127,7 @@ func (s *PyMLState) Fit(ctx *core.Context, bucket data.Array) (data.Value, error
 // will be updated by the data, the model is protected by Python's GIL. So,
 // this method doesn't require a write lock.
 func (s *PyMLState) fit(ctx *core.Context, bucket data.Array) (data.Value, error) {
-	return s.ins.Call("fit", bucket)
+	return s.base.Call("fit", bucket)
 }
 
 // FitMap receives `[]data.Map`, these maps are converted to `data.Array`
@@ -173,12 +137,9 @@ func (s *PyMLState) FitMap(ctx *core.Context, bucket []data.Map) (data.Value, er
 		args[i] = v
 	}
 
-	s.rwm.RLock() // Same as fit method. This doesn't have to be Lock().
+	s.rwm.RLock()
 	defer s.rwm.RUnlock()
-	if s.ins == nil {
-		return nil, ErrAlreadyTerminated
-	}
-	return s.ins.Call("fit", args)
+	return s.base.Call("fit", args)
 }
 
 // Predict applies the model to the data. It returns a result returned from
@@ -186,10 +147,7 @@ func (s *PyMLState) FitMap(ctx *core.Context, bucket []data.Map) (data.Value, er
 func (s *PyMLState) Predict(ctx *core.Context, dt data.Value) (data.Value, error) {
 	s.rwm.RLock()
 	defer s.rwm.RUnlock()
-	if s.ins == nil {
-		return nil, ErrAlreadyTerminated
-	}
-	return s.ins.Call("predict", dt)
+	return s.base.Call("predict", dt)
 }
 
 // Save saves the model of the state. pystate calls `save` method and
@@ -197,44 +155,14 @@ func (s *PyMLState) Predict(ctx *core.Context, dt data.Value) (data.Value, error
 func (s *PyMLState) Save(ctx *core.Context, w io.Writer, params data.Map) error {
 	s.rwm.RLock()
 	defer s.rwm.RUnlock()
-	if s.ins == nil {
-		return ErrAlreadyTerminated
+	if err := s.base.CheckTermination(); err != nil {
+		return err
 	}
 
 	if err := s.savePyMLMsgpack(w); err != nil {
 		return err
 	}
-
-	temp, err := ioutil.TempFile("", "sensorbee_py_ml_state") // TODO: TempDir should be configurable
-	if err != nil {
-		return fmt.Errorf("cannot create a temporary file for saving data: %v", err)
-	}
-	filepath := temp.Name()
-	if err := temp.Close(); err != nil {
-		ctx.ErrLog(err).WithField("filepath", filepath).Warn("Cannot close the temporary file")
-	}
-	defer func() {
-		if err := os.Remove(filepath); err != nil && !os.IsNotExist(err) {
-			ctx.ErrLog(err).WithField("filepath", filepath).Warn("Cannot remove the temporary file")
-		}
-	}()
-
-	_, err = s.ins.Call("save", data.String(filepath), params)
-	if err != nil {
-		return err
-	}
-
-	f, err := os.Open(filepath)
-	if err != nil {
-		return fmt.Errorf("cannot open the temporary file having the saved data: %v", err)
-	}
-	defer func() {
-		if err := temp.Close(); err != nil {
-			ctx.ErrLog(err).WithField("filepath", filepath).Warn("Cannot close the temporary file")
-		}
-	}()
-	_, err = io.Copy(w, f)
-	return err
+	return s.base.Save(ctx, w, params)
 }
 
 const (
@@ -247,35 +175,28 @@ func (s *PyMLState) savePyMLMsgpack(w io.Writer) error {
 	}
 
 	// Save parameter of PyMLState before save python's model
-	save := &pyMLMsgpack{
-		ModulePath: s.modulePath,
-		ModuleName: s.moduleName,
-		ClassName:  s.className,
-		BatchSize:  s.batchSize,
-	}
-
 	msgpackHandle := &codec.MsgpackHandle{}
 	var out []byte
 	enc := codec.NewEncoderBytes(&out, msgpackHandle)
-	if err := enc.Encode(save); err != nil {
+	if err := enc.Encode(&s.params); err != nil {
 		return err
 	}
 
-	// Write size of pyMLMsgpack
+	// Write size of MLParams
 	dataSize := uint32(len(out))
 	err := binary.Write(w, binary.LittleEndian, dataSize)
 	if err != nil {
 		return err
 	}
 
-	// Write pyMLMsgpack in msgpack
+	// Write MLParams in msgpack
 	n, err := w.Write(out)
 	if err != nil {
 		return err
 	}
 
 	if n < len(out) {
-		return errors.New("cannot save the pyMLMsgpack data")
+		return errors.New("cannot save the MLParams data")
 	}
 
 	return nil
@@ -286,8 +207,8 @@ func (s *PyMLState) savePyMLMsgpack(w io.Writer) error {
 func (s *PyMLState) Load(ctx *core.Context, r io.Reader, params data.Map) error {
 	s.rwm.Lock()
 	defer s.rwm.Unlock()
-	if s.ins == nil {
-		return ErrAlreadyTerminated
+	if err := s.base.CheckTermination(); err != nil {
+		return err
 	}
 
 	var formatVersion uint8
@@ -295,81 +216,46 @@ func (s *PyMLState) Load(ctx *core.Context, r io.Reader, params data.Map) error 
 		return err
 	}
 
-	// TODO: remove PyMLState specific parameters from params
+	// TODO: remove MLParams specific parameters from params
 
 	switch formatVersion {
 	case 1:
-		return s.loadPyMsgpackAndDataV1(ctx, r, params)
+		return s.loadMLParamsAndDataV1(ctx, r, params)
 	default:
 		return fmt.Errorf("unsupported format version of PyMLState container: %v", formatVersion)
 	}
 }
 
-func (s *PyMLState) loadPyMsgpackAndDataV1(ctx *core.Context, r io.Reader, params data.Map) error {
+func (s *PyMLState) loadMLParamsAndDataV1(ctx *core.Context, r io.Reader, params data.Map) error {
 	var dataSize uint32
 	if err := binary.Read(r, binary.LittleEndian, &dataSize); err != nil {
 		return err
 	}
 	if dataSize == 0 {
-		return errors.New("size of pyMLMsgpack must be greater than 0")
+		return errors.New("size of MLParams must be greater than 0")
 	}
 
-	// Read pyMLMsgpack from reader
+	// Read MLParams from reader
 	buf := make([]byte, dataSize)
 	n, err := r.Read(buf)
 	if err != nil {
 		return err
 	}
 	if n != int(dataSize) {
-		return errors.New("read size is different from pyMLMsgpack")
+		return errors.New("read size is different from the size of MLParams")
 	}
 
-	// Desirialize pyMLMsgpack
-	var saved pyMLMsgpack
+	// Desirialize MLParams
+	var saved MLParams
 	msgpackHandle := &codec.MsgpackHandle{}
 	dec := codec.NewDecoderBytes(buf, msgpackHandle)
 	if err := dec.Decode(&saved); err != nil {
 		return err
 	}
-
-	temp, err := ioutil.TempFile("", "sensorbee_py_ml_state") // TODO: TempDir should be configurable
-	if err != nil {
-		return fmt.Errorf("cannot create a temporary file to store the data to be loaded: %v", err)
-	}
-	filepath := temp.Name()
-	tempClosed := false
-	closeTemp := func() {
-		if tempClosed {
-			return
-		}
-		if err := temp.Close(); err != nil {
-			ctx.ErrLog(err).WithField("filepath", filepath).Warn("Cannot close the temporary file")
-		}
-		tempClosed = true
-	}
-	defer func() {
-		closeTemp()
-		if err := os.Remove(filepath); err != nil {
-			ctx.ErrLog(err).WithField("filepath", filepath).Warn("Cannot remove the temporary file")
-		}
-	}()
-	if _, err := io.Copy(temp, r); err != nil {
+	if err := s.base.Load(ctx, r, params); err != nil {
 		return err
 	}
-	closeTemp()
-
-	ins, err := newPyInstance("load", saved.ModulePath, saved.ModuleName, saved.ClassName, []data.Value{data.String(filepath), params}...)
-	if err != nil {
-		return err
-	}
-
-	// TODO: Support alternative load strategy.
-	// Currently, this method first loads a new model, and then release the old one.
-	// However, releasing the old model before loading the new model is sometimes
-	// required to reduce memory consumption. It should be configurable.
-
-	// Exchange instance in `s` when Load succeeded
-	s.set(ins, saved.ModulePath, saved.ModuleName, saved.ClassName, saved.BatchSize)
+	s.params = saved
 	return nil
 }
 
